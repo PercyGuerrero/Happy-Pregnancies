cdac1987f66a1a82f04e5537785c37631ba132fe
PercyGuerrero <percypgl1997@gmail.com>
2017-06-18 10:57:47 -0500

    Re

---
+++ Master/Happy1/Happy1/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("Happy1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Happy1")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("9d5effe7-e642-4b17-abf6-76247cbc34c1")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Puede especificar todos los valores o aceptar los valores predeterminados de los números de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Master/Happy1/Happy1/obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Principal.Master" CodeBehind="Default.aspx.vb" Inherits="Happy1._Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <link href="Estilos/Inicio.css" rel="stylesheet" />
+    <section>
+
+        <div>
+            <h2 class="subTitulo">Plan Pre embarazo</h2>
+            <div class="circulo">
+            </div>
+
+            <section>
+                <a class="masInfo" href="">Mas informacion</a>
+            </section>
+
+        </div>
+        <div>
+            <h2 class="subTitulo">Plan embarazo</h2>
+            <div class="circulo">
+            </div>
+
+            <section>
+                <a class="masInfo" href="">Mas informacion</a>
+            </section>
+
+        </div>
+        <div>
+            <h2 class="subTitulo">Plan pos embarazo</h2>
+            <div class="circulo">
+            </div>
+            <section>
+                <a class="masInfo" href="">Mas informacion</a>
+            </section>
+
+        </div>
+
+    </section>
+</asp:Content>
